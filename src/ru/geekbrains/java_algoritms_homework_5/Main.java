78462c73ddf529a222f373a753166706cf92fd76
lekoal <coreglus@gmail.com>
2021-09-21 10:51:51 +0300

    Exponentiation method added

--- src/ru/geekbrains/java_algoritms_homework_5/Main.java
+++ src/ru/geekbrains/java_algoritms_homework_5/Main.java
@@ -3,8 +3,13 @@ package ru.geekbrains.java_algoritms_homework_5;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(exponentiation(3, 7)); // Вызов метода возведения в степень
     }
 
-    
+    public static int exponentiation(int a, int x) { // Метод возведения в степень
+        if (x == 2) {
+            return a * a;
+        }
+        return exponentiation(a, x - 1) * a;
+    }
 }
